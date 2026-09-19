d66be8a796d3fc9a0c2009fca6bde44820a4ea75
alanbanasiak3-create <alanbanasiak3@gmail.com>
2026-09-19 14:49:23 +0200

    Add files via upload

---
+++ map_postals/fxmanifest.lua
@@ -0,0 +1,16 @@
+-- Leaked by Crystal Leaks
+-- https://discord.gg/8z2HGQ36CA
+
+fx_version 'cerulean'
+game 'gta5'
+this_is_a_map 'yes'
+
+-- Files.
+files {
+    "MINIMAP_LOADER.gfx"    -- Always first in the list.
+}
+
+client_scripts {
+    "config.lua",
+	"client.lua"
+}
